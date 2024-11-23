710e8832e847631c740d5cb6fa62facb711680ab
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-11-23 12:33:34 -0300

    feat: initial commit

---
+++ src/main/java/br/com/patrick/mailservice/MailserviceApplication.java
@@ -0,0 +1,15 @@
+package br.com.patrick.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class MailserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailserviceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/patrick/mailservice/services/FileOSService.java
@@ -0,0 +1,41 @@
+package br.com.patrick.mailservice.services;
+
+import br.com.patrick.mailservice.config.OSClientConfiguration;
+import com.oracle.bmc.objectstorage.requests.GetObjectRequest;
+import com.oracle.bmc.objectstorage.responses.GetObjectResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+
+@Service
+public class FileOSService {
+    String bucketName = "BUCKET";
+    String namespace = "NAMESPACE";
+
+    @Autowired
+    private OSClientConfiguration clientConfiguration;
+
+    public String getReportFileContent(String filename){
+        GetObjectRequest objectRequest = GetObjectRequest.builder()
+                .namespaceName(namespace)
+                .bucketName(bucketName)
+                .objectName(filename)
+                .build();
+
+        try {
+            GetObjectResponse objectResponse = clientConfiguration.getObjectStorage().getObject(objectRequest);
+            InputStream inputStream = objectResponse.getInputStream();
+
+            String content = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining());
+            return content;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return "";
+    }
+}
